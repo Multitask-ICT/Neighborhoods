626817c65db3605e77b4203c9786073d2b938e7a
Jesse van Assen <jesse.v.assen@gmail.com>
2013-07-24 15:11:02 +0200

    Refactored the importer to use the more standard selects and inserts when the row doesn't exist, instead of INSERT OR IGNORE.

--- neighborhoods/importer/cbs_importer.py
+++ neighborhoods/importer/cbs_importer.py
@@ -33,7 +33,7 @@ class CBSImporter():
 
     def import_area(self, shapefile_reader, area_type, has_parent):
         cursor = self._connection.cursor()
-        cursor.execute("INSERT OR IGNORE INTO AreaType (AreaType) VALUES (?)", (area_type,))
+        area_type_id = self._attach_area_type(area_type, cursor)
 
         for record, shape in zip(shapefile_reader.iterRecords(), shapefile_reader.iterShapes()):
             code, name = record[0].strip(), record[1].strip()
@@ -42,21 +42,30 @@ class CBSImporter():
             if len(name) == 0 or self._area_exists(code, cursor):
                 continue
 
-            area_id = self._insert_area(name, code, area_type, parent_code, cursor)
+            area_id = self._insert_area(name, code, area_type_id, parent_code, cursor)
             shape_id = self._insert_shape(area_id, cursor)
             self._insert_coordinates(shape_id, shape.points, cursor)
 
         self._connection.commit()
 
+    def _attach_area_type(self, area_type, cursor):
+        cursor.execute("SELECT AreaTypeId FROM AreaType WHERE AreaType = ?", (area_type,))
+        area_type_id = cursor.fetchone()
+        if area_type_id is not None:
+            return area_type_id[0]
+        else:
+            cursor.execute("INSERT INTO AreaType (AreaType) VALUES (?)", (area_type,))
+            return cursor.lastrowid
+
     def _area_exists(self, area_code, cursor):
         cursor.execute("SELECT EXISTS(SELECT Code FROM Area WHERE Code = ?)", (area_code,))
         return cursor.fetchone()[0] > 0
 
-    def _insert_area(self, name, code, area_type, parent_code, cursor):
+    def _insert_area(self, name, code, area_type_id, parent_code, cursor):
         cursor.execute(
             "INSERT INTO Area (Name, Code, AreaTypeId, ParentAreaId) "
-            "SELECT ?, ?, (SELECT AreaTypeId FROM AreaType WHERE AreaType = ?), (SELECT AreaId FROM Area WHERE Code = ?)",
-            (name, code, area_type, parent_code)
+            "SELECT ?, ?, ?, (SELECT AreaId FROM Area WHERE Code = ?)",
+            (name, code, area_type_id, parent_code)
         )
         return cursor.lastrowid
 
@@ -72,13 +81,30 @@ class CBSImporter():
         for point_index in range(len(points)):
             rd_coordinate = RD(*points[point_index])
             geocode_coordinate = convert_to_geocode_visitor.visit_rd(rd_coordinate)
-            cursor.execute(
-                "INSERT OR IGNORE INTO Coordinate (Geocode_Latitude, Geocode_Longitude, RD_X, RD_Y) VALUES (?, ?, ?, ?)",
-                tuple(geocode_coordinate) + tuple(rd_coordinate)
-            )
+
+            coordinate_id = self._attach_coordinate(geocode_coordinate, rd_coordinate, cursor)
 
             cursor.execute(
                 "INSERT INTO ShapeCoordinate (ShapeId, CoordinateId, Ordering) "
-                "SELECT ?, (SELECT CoordinateId FROM Coordinate WHERE (Geocode_Latitude = ? AND Geocode_Longitude = ?) OR (RD_X = ? AND RD_Y = ?) LIMIT 1), ?",
-                (shape_id,) + tuple(geocode_coordinate) + tuple(rd_coordinate) + (point_index,)
+                "SELECT ?, ?, ?",
+                (shape_id, coordinate_id, point_index)
             )
+
+    def _attach_coordinate(self, geocode_coordinate, rd_coordinate, cursor):
+        cursor.execute(
+            "SELECT CoordinateId FROM Coordinate "
+            "WHERE (Geocode_Latitude = ? AND Geocode_Longitude = ?) "
+            "OR    (RD_X = ? AND RD_Y = ?) "
+            "LIMIT 1",
+            tuple(geocode_coordinate) + tuple(rd_coordinate)
+        )
+        coordinate_id = cursor.fetchone()
+        if coordinate_id is not None:
+            return coordinate_id[0]
+        else:
+            cursor.execute(
+                "INSERT INTO Coordinate (Geocode_Latitude, Geocode_Longitude, RD_X, RD_Y) "
+                "VALUES (?, ?, ?, ?)",
+                tuple(geocode_coordinate) + tuple(rd_coordinate)
+            )
+            return cursor.lastrowid
